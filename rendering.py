f86d11f32ca5f90e24a51f21bea07c2f8beb1541
Justin Kunimune <justinkunimune@gmail.com>
2019-06-20 09:35:02 -0400

    Practising scales
    Debug arrows now scale with their quantitys' magnitudes.

--- rendering.py
+++ rendering.py
@@ -9,12 +9,17 @@ X_AX_ROTATION = Quaternion(axis=[1,0,0], degrees=90)
 Y_AX_ROTATION = Quaternion(axis=[0,1,0], degrees=90)
 Z_AX_ROTATION = Quaternion(axis=[0,0,1], degrees=90)
 
+UNIT_SCALE = .001
+VELOCITY_SCALE = .02
+ANGULARV_SCALE = .002
+
+
 class Actor():
 	def __init__(self, model, mesh_readers={}):
 		model_directory, model_name = model.split("->")
 		if model_directory not in mesh_readers:
 			mesh_readers[model_directory] = rc.WavefrontReader("Meshes/{}.obj".format(model_directory))
-		self.mesh = mesh_readers[model_directory].get_mesh(model_name, scale=.001, mean_center=False)
+		self.mesh = mesh_readers[model_directory].get_mesh(model_name, scale=UNIT_SCALE, mean_center=False)
 		self.mesh.rotation = rc.coordinates.RotationQuaternion(1, 0, 0, 0)
 
 	def update(self, t):
@@ -57,12 +62,14 @@ class VectorActor(Actor):
 				assign_wxyz(self.mesh.rotation, [0,0,0,0])
 			else:
 				assign_wxyz(self.mesh.rotation, Quaternion(axis=[v[2],0,-v[0]], angle=np.arccos(v[1]/np.linalg.norm(v))))
+			self.mesh.scale.y = VELOCITY_SCALE*np.linalg.norm(v)
 		elif self.quantity == "angularv":
 			ω = self.body.get_angularv(t)
 			if ω[0] == 0 and ω[2] == 0:
 				assign_wxyz(self.mesh.rotation, [0,0,0,0])
 			else:
 				assign_wxyz(self.mesh.rotation, Quaternion(axis=[ω[2],0,-ω[0]], angle=np.arccos(ω[1]/np.linalg.norm(ω))))
+			self.mesh.scale.y = ANGULARV_SCALE*np.linalg.norm(ω)
 		else:
 			raise ValueError("Unrecognised vector quantity: {}".format(self.quantity))
 
--- sat_model.py
+++ sat_model.py
@@ -10,6 +10,10 @@ from physics import RigidBody, Environment
 from rendering import BodyActor, VectorActor, Stage
 
 
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
 dipole_moment = np.array([0, 0, 0]) # A*m^2
 B_earth = np.array([35e-6, 0, 0]) # T
 
@@ -26,7 +30,7 @@ if __name__ == '__main__':
 	m = 0.05223934 # kg
 	cm = [0.00215328, -0.00860001, -0.00038142] # m --> check coordinates
 	# cm = [0, 0, .1]
-	v0 = [0,0, 0] # m/s
+	v0 = [.04,.02,-.01] # m/s
 	ω0 = [.3, 1, -.6] # rad/s
 
 	satellite_l = RigidBody(I, m, cm, init_position=[-.1,0,0], init_velocity=v0, init_angularv=ω0)
@@ -47,13 +51,13 @@ if __name__ == '__main__':
 		BodyActor(satellite_r, "ThinSatFrame->Frame"),
 		# VectorActor(satellite_l, "Resources/arrow->Arrow", "xaxis"),
 		# VectorActor(satellite_l, "Resources/arrow->Arrow", "yaxis"),
-		# VectorActor(satellite_l, "Resources/arrow->Arrow", "zaxis")
-		# VectorActor(satellite_l, "Resources/arrow->Arrow", "velocity")
-		VectorActor(satellite_l, "Resources/arrow->Arrow", "angularv")
-		], environment)
+		# VectorActor(satellite_l, "Resources/arrow->Arrow", "zaxis"),
+		VectorActor(satellite_l, "Resources/arrow->Arrow", "velocity"),
+		VectorActor(satellite_l, "Resources/arrow->Arrow", "angularv"),
+	], environment)
 
 
-	window = pyglet.window.Window()
+	window = pyglet.window.Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 	scene = rc.Scene(
 			meshes=[a.mesh for a in stage.actors],
 			camera=rc.Camera(position=(0, 0, .4)),
